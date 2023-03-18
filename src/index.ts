c0dccddbd5ae9605178e65e3fe5e3143e001d52f
sajico <sajico@we-love-cats.net>
2023-03-19 03:12:29 +0900

    全貌把握

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export * from './meow';
-export const meow1 = () => console.log('にゃ〜ん1');
-const meow2 = () => console.log('にゃ〜ん2');
-export default meow2;
-const meow3 = () => console.log('にゃ〜ん3');
-module.exports = meow3;
-const meow4 = () => console.log('にゃ〜ん4');
-module.exports.default = meow4;
+import { meow } from './meow';
+
+export namespace LovelyCat {
+    export const meow = (arg: string): string => `にゃ〜ん! arg:${arg}`;
+}
+export const meow2 = meow;
+
+module.exports.default = { LovelyCat, meow2 };
--- src/meow.ts
+++ src/meow.ts
@@ -1,7 +1,1 @@
-export const meow5 = () => console.log('にゃ〜ん5');
-const meow6 = () => console.log('にゃ〜ん6');
-export default meow6;
-const meow7 = () => console.log('にゃ〜ん7');
-module.exports = meow7;
-const meow8 = () => console.log('にゃ〜ん8');
-module.exports.default = meow8;
+export const meow = (arg: string): string => `にゃ〜ん arg:${arg}`;
