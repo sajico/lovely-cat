aa80c6781f10ac8f08914df681026e301dbed82a
sajico <sajico@we-love-cats.net>
2023-03-19 01:01:10 +0900

    functionで定義

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 export * from './meow';
-export const meow1 = () => console.log('にゃ〜ん1');
-const meow2 = () => console.log('にゃ〜ん2');
+
+export function meow1() {
+    console.log('にゃ〜ん1');
+}
+
+function meow2() {
+    console.log('にゃ〜ん2');
+}
 export default meow2;
+
 const meow3 = () => console.log('にゃ〜ん3');
 module.exports = meow3;
+
 const meow4 = () => console.log('にゃ〜ん4');
 module.exports.default = meow4;
--- src/meow.ts
+++ src/meow.ts
@@ -1,7 +1,14 @@
-export const meow5 = () => console.log('にゃ〜ん5');
-const meow6 = () => console.log('にゃ〜ん6');
+export function meow5() {
+    console.log('にゃ〜ん5');
+}
+
+function meow6() {
+    console.log('にゃ〜ん6');
+}
 export default meow6;
+
 const meow7 = () => console.log('にゃ〜ん7');
 module.exports = meow7;
+
 const meow8 = () => console.log('にゃ〜ん8');
 module.exports.default = meow8;
