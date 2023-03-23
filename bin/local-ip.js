833364781b3893538fddf37a9e81808267c51cfc
sajico <sajico@we-love-cats.net>
2023-03-23 18:47:41 +0900

    local-ip実装

--- bin/local-ip.js
+++ bin/local-ip.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
 "use strict";
-console.log('にゃ〜ん');
+Object.defineProperty(exports, "__esModule", { value: true });
+const os_1 = require("os");
+const getIpAddress = () => {
+    var _a;
+    const nets = (0, os_1.networkInterfaces)();
+    const net = (_a = nets['en0']) === null || _a === void 0 ? void 0 : _a.find(v => v.family === 'IPv4');
+    return !!net ? net.address : null;
+};
+console.log(getIpAddress());
 //# sourceMappingURL=local-ip.js.map
--- src/bin/local-ip.ts
+++ src/bin/local-ip.ts
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log('にゃ〜ん');
+import { networkInterfaces } from 'os';
+
+const getIpAddress = (): string | null => {
+    const nets = networkInterfaces();
+    const net = nets['en0']?.find(v => v.family === 'IPv4');
+    return !!net ? net.address : null;
+};
+console.log(getIpAddress());
